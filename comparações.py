5a29d7660589f632e5ff6e061dc8a6581633643c
Caue Finger <fingercaue@gmail.com>
2025-07-14 21:57:30 -0300

    Alterando texto

--- comparações.py
+++ comparações.py
@@ -5,7 +5,7 @@ n1 = int(input("Digite um número: "))
 n2 = int(input("digite mais um número: "))
  
 x = n1 == n2
-print ("são iguais? ", x, "\n")
+print ("Os números são iguais? ", x, "\n")
 
 z = n1 > n2
 print (n1, "é maior que", n2, " ? ", z, " \n " )   
