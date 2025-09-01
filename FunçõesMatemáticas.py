a51e9274f3c298d6e1e79c61bdc131a9fc1aa479
Caue Finger <fingercaue@gmail.com>
2025-09-01 19:30:23 -0300

    funções built-in

---
+++ FunçõesMatemáticas.py
@@ -0,0 +1,22 @@
+# Funções matemáticas built-in e o módulo math 
+# o Python possui 2 grandes tipos de funções matemáticas, o built-in (que não precisa importar nada) e o módulo math
+# FUNÇÕES BUILT-IN
+
+valores = [13,1,12,-23,-3,11]
+print(max(valores)) 
+# imprime o maior valor da lista de valores
+print(min(valores))
+# imprime o menor valor da lista de valores 
+a = -5
+print(abs(a))
+# exibe o valor absoluto de -5 (a)
+b = 8
+print(abs(b))
+
+# função de exponenciação (power)
+print(pow(a,b))
+# -5 elevado a 8
+
+c = 21.234245
+print(round(c,3))
+# função round serve para arredondar, escolhendo a qtd de casas após a vírgula 
